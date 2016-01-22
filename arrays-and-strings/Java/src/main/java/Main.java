c155892b8eed74591b763f94abe75767bdaee690
Shafeen M <sodl02@gmail.com>
2016-01-21 22:14:58 -0500

    added alternate solution for Arrays and Strings - 1.1 this solution could not use any additional data structures

--- arrays-and-strings/Java/src/main/java/Main.java
+++ arrays-and-strings/Java/src/main/java/Main.java
@@ -16,7 +16,8 @@ public class Main {
     // [1.1] Implement an algorithm to determine if a string has all unique characters. What
     //       if you cannot use additional data structures?
     public static void q1_1(String str) {
-        System.out.printf("\"%s\" has all unique characters = %b", str, hasAllUniqueChars(str));
+        System.out.printf("\"%s\" has all unique characters = %b\n", str, hasAllUniqueChars(str));
+        System.out.printf("\"%s\" has all unique characters = %b\n", str, hasAllUniqueChars_nohashmap(str));
     }
 
     // [1.1] helper
@@ -33,5 +34,16 @@ public class Main {
 
     // [1.1] w/o addition data structures -
     // for each char in string, compare with every other char for duplicates
+    public static boolean hasAllUniqueChars_nohashmap(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            for (int j = i + 1; j < str.length(); j++) {
+                if (c == str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
 }
