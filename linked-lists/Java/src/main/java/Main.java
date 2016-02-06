bed1229c948edf152ce73eafd3be8ba202e013c8
Shafeen M <sodl02@gmail.com>
2016-02-06 00:38:46 -0500

    added solution for Linked Lists - 2.1 TODO: there is an optional FOLLOW UP question for 2.1

--- linked-lists/Java/src/main/java/Main.java
+++ linked-lists/Java/src/main/java/Main.java
@@ -1,15 +1,55 @@
+import java.util.HashSet;
+
 /**
  * CTCI - Linked Lists.
  */
 public class Main {
+
     public static void main(String[] args) {
-        // q2_1();
+        q2_1(getUnsortedLinkedList());
     }
+
+    public static LinkedList<Integer> getUnsortedLinkedList() {
+        LinkedList<Integer> unsortedList = new LinkedList<Integer>(1);
+        unsortedList.setNext(new LinkedList<Integer>(3)).getNext()
+                .setNext(new LinkedList<Integer>(2)).getNext()
+                .setNext(new LinkedList<Integer>(2)).getNext()
+                .setNext(new LinkedList<Integer>(4)).getNext()
+                .setNext(new LinkedList<Integer>(4)).getNext()
+                .setNext(new LinkedList<Integer>(2)).getNext()
+                .setNext(new LinkedList<Integer>(3));
+        return unsortedList;
+    }
+
+    // -----------------------------------------------
 
     // [2.1] Write code to remove duplicates from an unsorted linked list.
     // FOLLOW UP (OPTIONAL)
     // How would you solve this problem if a temporary buffer is not allowed?
-    public void q2_1() {
-        // TODO: complete this
+    public static void q2_1(LinkedList<Integer> unsortedList) {
+        // print the list with and w/o the duplicates
+        System.out.println(unsortedList);
+        System.out.println(getListWithoutDuplicates(unsortedList));
     }
+
+    // [2.1] helper
+    public static LinkedList<Integer> getListWithoutDuplicates(LinkedList<Integer> list) {
+        LinkedList<Integer> prev = null;
+        LinkedList<Integer> head = list;
+        HashSet<Integer> integerSet = new HashSet<Integer>();
+        while (head != null) {
+            if (!integerSet.contains(head.getData())) {
+                integerSet.add(head.getData());
+                prev = head;
+            } else {
+                prev.setNext(head.getNext());
+            }
+            head = head.getNext();
+        }
+        return list;
+    }
+
+    // -----------------------------------------------
+
+
 }
