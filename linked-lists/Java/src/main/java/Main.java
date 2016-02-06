cb6467a3be91a4e0c43e2cc527905be355d560d8
Shafeen M <sodl02@gmail.com>
2016-02-05 23:41:55 -0500

    started CTCI chapter 2 (LinkedLists) problems

---
+++ linked-lists/Java/src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * CTCI - Linked Lists.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // q2_1();
+    }
+
+    // [2.1] Write code to remove duplicates from an unsorted linked list.
+    // FOLLOW UP (OPTIONAL)
+    // How would you solve this problem if a temporary buffer is not allowed?
+    public void q2_1() {
+        // TODO: complete this
+    }
+}
